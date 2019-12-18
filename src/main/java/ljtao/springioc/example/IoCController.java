549590cc362409dbe99299df2e4fbbd63120dfd2
妖风伴残月 <43426976+mathLjtao@users.noreply.github.com>
2019-12-18 17:01:11 +0800

    com

---
+++ src/main/java/ljtao/springioc/example/IoCController.java
@@ -0,0 +1,9 @@
+package ljtao.springioc.example;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class IoCController {
+    private Map<String,Object> beanMap=new ConcurrentHashMap<>();
+    
+}
