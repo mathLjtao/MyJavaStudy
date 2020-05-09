9294a41241268c8057131e6d572e93f0cdc07f01
锦涛 <43426976+mathLjtao@users.noreply.github.com>
2020-05-09 15:25:34 +0800

    com

---
+++ src/main/java/ljtao/javase/inner/a_basic/Demo01.java
@@ -0,0 +1,49 @@
+package ljtao.javase.inner.a_basic;
+
+/**
+ * @author ljtao3 on 2020/5/8
+ */
+public class Demo01 {
+    private String name="ljtao";
+    private int age=18;
+    private static String photo="188";
+
+
+    class Inner01{
+        private String name="ljtao2";
+        public void fun1(){
+            System.out.println("使用内部类自己的属性："+name);
+            System.out.println("使用外部类的属性："+Demo01.this.name);
+            System.out.println(age);
+        }
+    }
+    static class Inner02{
+        private String name="ljtao2";
+        public  void fun1(){
+            System.out.println(name);
+            //System.out.println(age);
+        }
+    }
+    static class Inner03{
+        private String name="ljtao2";
+        public static void fun1(){
+            //System.out.println(name);
+            //System.out.println(age);
+        }
+    }
+
+    public void fun1(){
+        //如果不是静态方法，可以这样调用
+        new Inner01().fun1();
+        new Inner02().fun1();
+        Inner03.fun1();
+    }
+    public static void main(String[] args) {
+
+        //静态方法访问的方式
+        new Demo01().new Inner01().fun1();
+        new Inner02().fun1();
+        Inner03.fun1();;
+    }
+
+}
---
+++ src/main/java/ljtao/javase/inner/a_basic/ex2/Games.java
@@ -0,0 +1,46 @@
+package ljtao.javase.inner.a_basic.ex2;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+/**
+ * @author ljtao3 on 2020/5/8
+ */
+interface Game{ boolean move();}
+interface GameFactory{Game getGame();}
+class Checkers implements  Game{
+    private int move=0;
+    private final  static int MOVES=3;
+    @Override
+    public boolean move() {
+        System.out.println("Checkers move "+move);
+        return ++move!=MOVES;
+    }
+    public static GameFactory factory=new GameFactory(){
+        @Override
+        public Game getGame() {
+            return new Checkers();
+        }
+    };
+}
+class Chess implements  Game{
+    private int move=0;
+    private final  static int MOVES=4;
+    @Override
+    public boolean move() {
+        System.out.println("Chess move "+move);
+        return ++move!=MOVES;
+    }
+    //Java8 Lambda 表达式,运用函数式接口
+    public static GameFactory factory=()->new Chess();
+}
+public class Games {
+    public static void palyGame(GameFactory factory){
+        Game s=factory.getGame();
+        while (s.move());
+    }
+
+    public static void main(String[] args) {
+        palyGame(Checkers.factory);
+        palyGame(Chess.factory);
+    }
+}
